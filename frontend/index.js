fd0932fe631549a997d9cd670516ab428088530c
Beatriz Ferreira dos Santos <bfsantos@RDM503728.local>
2020-11-24 18:13:30 -0300

    adicionando readme

--- frontend/index.js
+++ frontend/index.js
@@ -3,9 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/index';
+import App from './App';
 import {name as appName} from './app.json';
-import Login from './src/pages/login/login-page';
-import UserPage from './src/pages/user-student-page/user-page';
 
-AppRegistry.registerComponent(appName, () => UserPage);
+AppRegistry.registerComponent(appName, () => App);
